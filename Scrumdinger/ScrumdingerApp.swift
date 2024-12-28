c52320cbc5c26a85ce1336a009e54e43ba0344fe
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2024-12-28 22:27:51 +0300

    Add data persistence

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -10,11 +10,28 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     
+    @StateObject private var store = ScrumStore()
+    
     @State private var scrums = DailyScrum.sampleData
     
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: $scrums)
+            ScrumsView(scrums: $store.scrums) {
+                Task {
+                    do {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+                .task {
+                    do {
+                        try await store.load()
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
         }
     }
 }
